9312977a8add71aaba1188c7df39c1867f2edf5c
Tim <46413981+timcodejs@users.noreply.github.com>
2024-02-26 19:27:24 +0900

    앱 실행 시 - 이벤트 팝업 적용

---
+++ src/Components/EventPopup.tsx
@@ -0,0 +1,95 @@
+import React, {useEffect} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import {Color} from '../Utility/utils/Color';
+import {IconCrossIcon} from '../Utility/utils/SVG';
+import {PretendardBold} from '../Utility/utils/CustomFont';
+import {hp, screenHeight, screenWidth} from '../Utility/utils/UI';
+
+interface Props {
+  setIsPopup: (e: boolean) => void;
+}
+
+const EventPopup = ({setIsPopup}: Props) => {
+  const yPosition = useSharedValue(screenHeight);
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      yPosition.value = withSpring(0, {
+        damping: 20,
+        overshootClamping: true,
+      });
+    }, 300);
+    return () => {
+      clearTimeout(time);
+    };
+  }, []);
+
+  const aninatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: yPosition.value}],
+    };
+  });
+
+  return (
+    <View style={styles.wrap}>
+      <TouchableOpacity
+        style={styles.void}
+        onPress={() => {
+          setIsPopup(false);
+        }}
+      />
+      <Animated.View style={[styles.view, aninatedStyle]}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            setIsPopup(false);
+          }}>
+          <IconCrossIcon
+            color={Color.black}
+            width={30}
+            height={30}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <View style={styles.inner}>
+          <PretendardBold children={'Test Popup'} />
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+export default EventPopup;
+
+const styles = StyleSheet.create({
+  wrap: {
+    position: 'absolute',
+    zIndex: 9999999,
+    width: screenWidth,
+    height: screenHeight,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  void: {
+    width: screenWidth,
+    height: screenHeight,
+  },
+  view: {
+    position: 'absolute',
+    bottom: 0,
+    width: screenWidth,
+    height: hp(250),
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: Color.white,
+  },
+  inner: {
+    alignItems: 'center',
+  },
+  btn: {width: 30, height: 30},
+  icon: {margin: 10},
+});
--- src/Screens/Routes.tsx
+++ src/Screens/Routes.tsx
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Splash from '../Utility/utils/Splash';
+import EventPopup from '../Components/EventPopup';
 import BottomNavigation from '../Navigation/BottomNavigation';
 
 const queryClient = new QueryClient();
 
 const Routes = () => {
   const [isSplash, setIsSplash] = useState(true);
+  const [isPopup, setIsPopup] = useState(true);
 
   // 스플래시
   setTimeout(() => {
@@ -19,6 +21,7 @@ const Routes = () => {
   ) : (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{flex: 1}}>
+        {isPopup && <EventPopup setIsPopup={setIsPopup} />}
         <BottomNavigation />
       </GestureHandlerRootView>
     </QueryClientProvider>
